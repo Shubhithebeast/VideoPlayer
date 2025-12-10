39f2ff33fa9fa949ad25addd101acd575f0681e5
sbisht <sbisht@opentext.com>
2025-12-10 23:52:37 +0530

    utils - api error, response , asynchandler wrapper

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,24 @@
 import connectDB from './database/db.js';
+import {app} from './app.js';
 
 import dotenv from 'dotenv';
 dotenv.config({
     path: './.env'
 });
  
-connectDB();
+connectDB().then(() => {
+    app.on("error", (error) => {
+        console.log('Error in server😥:', error);
+        throw error;
+    });
+    app.listen(process.env.PORT || 8000 , () => {
+        console.log(`Server is running on port ${process.env.PORT}🚀...`);
+    });
+    
+}).catch((error) => {
+    console.log('Failed to connect to the database:', error);
+    process.exit(1);
+});   
 
 /*
 import express from 'express';
