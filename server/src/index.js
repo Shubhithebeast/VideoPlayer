77d1c03f1b144631e44209325fe1681d3594efc6
sbisht <sbisht@opentext.com>
2025-12-10 22:43:35 +0530

    mongodb setup

---
+++ server/src/index.js
@@ -0,0 +1,36 @@
+import connectDB from './database/db.js';
+
+import dotenv from 'dotenv';
+dotenv.config({
+    path: './.env'
+});
+ 
+connectDB();
+
+/*
+import express from 'express';
+const app = express();
+
+// IIFE to connect to MongoDB
+(async () => {
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        console.log('Connected to MongoDB successfully😊...');
+        app.on("error", (error) => {
+            console.log('Error in server😥:', error);
+            throw error;
+        });
+
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port ${process.env.PORT}🚀...`);
+        });
+    }catch(error){
+        console.log('Error connecting to MongoDB😥:', error);
+        throw error;
+    }
+})()
+
+*/
+
+
+
