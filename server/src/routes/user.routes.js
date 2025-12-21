3d5025ca1ec55afd1c888b8319014db5f47495c3
sbisht <sbisht@opentext.com>
2025-12-21 19:26:19 +0530

    feat: add user profile routes for fetching user data and updating account details

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import { changeCurrentUserPassword, getCurrentUser, loginUser, logoutUser, refreshAccessToken, registerUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js";
 import { verifyJWT } from './../middlewares/auth.middleware.js';
 
@@ -11,9 +11,13 @@ router.route("/register").post(
         {name:"coverImage", maxCount:1}
     ])
     ,registerUser);
-
 router.route("/login").post(upload.none(), loginUser);
 router.route("/logout").post(upload.none(), verifyJWT, logoutUser);
 router.route("/refreshToken").post(upload.none(), refreshAccessToken);
+router.route("/getUser").get(upload.none(), getCurrentUser);
+router.route("/changePassword").post(upload.none(), changeCurrentUserPassword);
+router.route("/updateAccountDetails").post(upload.none(), updateAccountDetails);
+router.route("/updateAvatar").post(upload.none(), updateUserAvatar);
+router.route("/updateCoverImage").post(upload.none(), updateUserCoverImage);
 
 export default router;
